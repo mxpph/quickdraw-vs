0307539c9996cd877eb29ccbaed12bc320098f7f
c ttps <67242476+cttps@users.noreply.github.com>
2024-07-23 13:33:20 -0700

    Add paperclip datapoint and serve first json obj

--- recreate-datapoint/fastapiserver.py
+++ recreate-datapoint/fastapiserver.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
@@ -6,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/getFirst")
+async def get_first_entry():
+    with open("full_raw_paperclip.ndjson", "r") as file:
+        firstLine = file.readline()
+        if firstLine:
+            return json.loads(firstLine)
+        else:
+            return {"error": "File is empty"}
