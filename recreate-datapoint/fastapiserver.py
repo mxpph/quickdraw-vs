459f5940d4e373d7304dfeaf151c3b7b96955237
c ttps <67242476+cttps@users.noreply.github.com>
2024-07-23 14:06:02 -0700

    Add basic interface and html serving

--- recreate-datapoint/fastapiserver.py
+++ recreate-datapoint/fastapiserver.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 import json
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    with open("static/index.html", "r") as file:
+        content = file.read()
+    return HTMLResponse(content=content)
 
 @app.get("/getFirst")
 async def get_first_entry():
