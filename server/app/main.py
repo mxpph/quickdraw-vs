059a3c162b6d182a8e143348b8a3d984f70c0016
mxpph <52426524+mxpph@users.noreply.github.com>
2024-07-28 23:57:07 +0200

    Await responses from Redis

--- server/app/main.py
+++ server/app/main.py
@@ -51,21 +51,21 @@ async def websocket_endpoint(websocket: WebSocket, game_id: str, player_id: str)
     if not redis_client:
         logging.error("game: Redis is not available")
         raise WebSocketException(code=status.WS_1011_INTERNAL_ERROR)
-    game_data = redis_client.get(f"game:{game_id}")
+    game_data = await redis_client.get(f"game:{game_id}")
     if not game_data:
         raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
-    players = redis_client.lrange(f"game:{game_id}:players", 0, -1)
+    players = await redis_client.lrange(f"game:{game_id}:players", 0, -1) # type: ignore
     if player_id not in players: # type: ignore
         raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
     await websocket.accept()
     logging.info("Websocket connection accepted.")
     try:
-        game_data = json.loads(redis_client.get(f"game:{game_id}")) # type: ignore
+        game_data = json.loads(await redis_client.get(f"game:{game_id}")) # type: ignore
         if game_data["status"] == "waiting":
-            nplayers = redis_client.llen(f"game:{game_id}:players")
+            nplayers = await redis_client.llen(f"game:{game_id}:players") # type: ignore
             while nplayers < 2: # type: ignore
                 await asyncio.sleep(1)
-                nplayers = redis_client.llen(f"game:{game_id}:players")
+                nplayers = await redis_client.llen(f"game:{game_id}:players") # type: ignore
             logging.info(f"More than 2 players found, starting")
             game_data["status"] = "playing"
             redis_client.set(f"game:{game_id}", json.dumps(game_data))
@@ -79,7 +79,7 @@ async def websocket_endpoint(websocket: WebSocket, game_id: str, player_id: str)
             pass
         await websocket.close()
     except WebSocketDisconnect:
-        redis_client.lrem(f"game:{game_id}:players", -1, player_id)
+        await redis_client.lrem(f"game:{game_id}:players", -1, player_id) # type: ignore
 
 @app.post("/create-game")
 async def create_game(data: GameData):
@@ -94,7 +94,7 @@ async def create_game(data: GameData):
             game_data = {
                 "status": "waiting",
             }
-            redis_client.set(f"game:{game_id}", json.dumps(game_data))
+            await redis_client.set(f"game:{game_id}", json.dumps(game_data))
             logging.info(f"Created new game with ID: {game_id}")
 
         player_id = str(uuid.uuid4())
@@ -102,7 +102,7 @@ async def create_game(data: GameData):
             "name": data.player_name,
             "id": player_id,
         }
-        redis_client.rpush(f"game:{game_id}:players", json.dumps(player_data))
+        await redis_client.rpush(f"game:{game_id}:players", json.dumps(player_data)) # type: ignore
         logging.info(f"Created new host player")
         url = f"{app.url_path_for('websocket_endpoint')}?game_id={game_id}&player_id={player_id}"
         return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
