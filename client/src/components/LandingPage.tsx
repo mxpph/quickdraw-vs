5c220f7a3a5d018f1dda5cf9be19230f72d029e8
c ttps <67242476+cttps@users.noreply.github.com>
2024-07-28 23:48:38 -0700

    Add form, fix theme

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -9,10 +9,14 @@ export default function LandingPage() {
     return(
         <div className="w-full">
             <h1 id="test" className="text-3xl w-full text-center font-semibold py-2">quickdraw vs</h1>
+            <form className="grid place-items-center shadow-md rounded px-8 pt-6 pb-8 mb-4 gap-2" id="gameForm">
+                <p>Game ID</p>
+                <input type="text" className='rounded-md p-2 bg-neutral-100 border-gray-500' id="game_id" name="game_id" required />
+                <p>Player Name</p>
+                <input type="text" className='rounded-md p-2 bg-neutral-100 border-gray-500' id="player_name" name="player_name" required />
+                <button type="submit" className="bg-blue-400 rounded-lg p-2">Create Game</button>
+            </form>
             <div className="w-full grid place-items-center mb-6">
-                <button className="place-items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4" id="newgame">
-                    New game
-                </button>
                 <div className="outline outline-2 outline-offset-2 outline-blue-500 rounded-xl overflow-hidden w-[90vw] shadow-xl grid place-items-center" id="canvasdiv">
                     <Canvas />
                 </div>
