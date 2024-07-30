109b62da6bed613f16861757bd981866d454ac41
mxpph <52426524+mxpph@users.noreply.github.com>
2024-07-30 19:08:33 +0200

    Increase top margin size

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -2,7 +2,7 @@ import GameForm from "./GameForm";
 
 export default function LandingPage() {
   return (
-    <div className="w-full grid place-items-center">
+    <div className="w-full grid place-items-center my-3">
       <h1 id="test" className="text-3xl w-full text-center font-semibold py-2">
         quickdraw vs
       </h1>
