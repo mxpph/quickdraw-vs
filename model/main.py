83d8e62c6744ea21777125eaa40fc74076117a32
c ttps <67242476+cttps@users.noreply.github.com>
2024-08-05 12:25:44 -0700

    Begin preprocessing steps using quickdraw dataset examples

---
+++ model/main.py
@@ -0,0 +1,94 @@
+import struct
+from struct import unpack
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+import cairocffi as cairo
+
+
+def unpack_drawing(file_handle):
+    key_id, = unpack('Q', file_handle.read(8))
+    country_code, = unpack('2s', file_handle.read(2))
+    recognized, = unpack('b', file_handle.read(1))
+    timestamp, = unpack('I', file_handle.read(4))
+    n_strokes, = unpack('H', file_handle.read(2))
+    image = []
+    for i in range(n_strokes):
+        n_points, = unpack('H', file_handle.read(2))
+        fmt = str(n_points) + 'B'
+        x = unpack(fmt, file_handle.read(n_points))
+        y = unpack(fmt, file_handle.read(n_points))
+        image.append((x, y))
+
+    return {
+        'key_id': key_id,
+        'country_code': country_code,
+        'recognized': recognized,
+        'timestamp': timestamp,
+        'image': image
+    }
+
+
+def unpack_drawings(filename):
+    path = os.getcwd()
+    path = os.path.join(path,"trainingdata",filename)
+    with open(path, 'rb') as f:
+        while True:
+            try:
+                yield unpack_drawing(f)
+            except struct.error:
+                break
+
+
+def vector_to_raster(vector_images, side=28, line_diameter=16, padding=16, bg_color=(0,0,0), fg_color=(1,1,1)):
+    """
+    padding and line_diameter are relative to the original 256x256 image.
+    """
+    
+    original_side = 256.
+    
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, side, side)
+    ctx = cairo.Context(surface)
+    ctx.set_antialias(cairo.ANTIALIAS_BEST)
+    ctx.set_line_cap(cairo.LINE_CAP_ROUND)
+    ctx.set_line_join(cairo.LINE_JOIN_ROUND)
+    ctx.set_line_width(line_diameter)
+
+    # scale to match the new size
+    # add padding at the edges for the line_diameter
+    # and add additional padding to account for antialiasing
+    total_padding = padding * 2. + line_diameter
+    new_scale = float(side) / float(original_side + total_padding)
+    ctx.scale(new_scale, new_scale)
+    ctx.translate(total_padding / 2., total_padding / 2.)
+
+    raster_images = []
+    for vector_image in vector_images:
+        # clear background
+        ctx.set_source_rgb(*bg_color)
+        ctx.paint()
+        
+        bbox = np.hstack(vector_image).max(axis=1)
+        offset = ((original_side, original_side) - bbox) / 2.
+        offset = offset.reshape(-1,1)
+        centered = [stroke + offset for stroke in vector_image]
+
+        # draw strokes, this is the most cpu-intensive part
+        ctx.set_source_rgb(*fg_color)        
+        for xv, yv in centered:
+            ctx.move_to(xv[0], yv[0])
+            for x, y in zip(xv, yv):
+                ctx.line_to(x, y)
+            ctx.stroke()
+
+        data = surface.get_data()
+        raster_image = np.copy(np.asarray(data)[::4])
+        raster_images.append(raster_image)
+    
+    return raster_images
+
+for drawing in unpack_drawings('full_binary_pencil.bin'):
+    print(vector_to_raster(drawing["image"])) # Image as set of point
+    # plt.imshow(np.asarray(drawing['image']), cmap='gray')
+    # plt.show()
+    break
