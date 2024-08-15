66cf463c75a20be54583a2a992583b6f857c7ea6
mxpph <52426524+mxpph@users.noreply.github.com>
2024-08-15 17:53:44 +0200

    Separate join and create forms on frontend

--- frontend/src/components/GameForm.tsx
+++ frontend/src/components/GameForm.tsx
@@ -3,25 +3,45 @@ import Cookies from 'js-cookie'
 
 export default function GameForm() {
 
-  const [formData, setFormData] = useState({
-    game_id: '',
+  const [createFormData, setCreateFormData] = useState({
     player_name: '',
     max_players: '',
     rounds: '',
   })
 
-  const handleChange = (e: any) => {
-    setFormData({
-      ...formData,
+  const [joinFormData, setJoinFormData] = useState({
+    game_id: '',
+    player_name: '',
+  })
+
+  const handleCreateFormChange = (e: any) => {
+    setCreateFormData({
+      ...createFormData,
       [e.target.name]: e.target.value,
     })
   }
 
-  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+  const handleJoinFormChange = (e: any) => {
+    setJoinFormData({
+      ...joinFormData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>, form: string) {
     event.preventDefault()
 
+    var url
+    var formData
+    if (form === "join") {
+      url = "/join-game"
+      formData = joinFormData
+    } else {
+      url = "/create-game"
+      formData = createFormData
+    }
     try {
-      const response = await fetch("http://localhost:3000/create-game", {
+      const response = await fetch(url, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -47,83 +67,112 @@ export default function GameForm() {
       console.error("Error:", error)
     }
   }
+
   return (
-    <form
-      id="gameform"
-      noValidate={true}
-      onSubmit={handleSubmit}
-      className="grid place-items-center shadow-md bg-neutral-50 rounded px-8 pt-6 pb-8 mb-4 gap-2"
-    >
+    <div className="w-full grid grid-cols-2 grid-rows-1 gap-4">
+      <form
+        id="createForm"
+        noValidate={true}
+        onSubmit={(e: any) => handleSubmit(e, "create")}
+        className="grid place-items-center shadow-md bg-neutral-50 rounded px-8 pt-6 pb-8 mb-4 gap-2"
+      >
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Player Name</span>
+          </div>
+          <input
+            type="text"
+            className="input input-bordered input-primary w-full max-w-xs"
+            name="player_name"
+            maxLength={16}
+            onChange={handleCreateFormChange}
+            required
+          />
+        </label>
+        <div className="w-full grid grid-cols-2 grid-rows-1 gap-2">
+          <select
+            className="select select-primary"
+            name="max_players"
+            onChange={handleCreateFormChange}
+            form="createForm"
+          >
+            <option disabled selected>
+              Max players
+            </option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+          </select>
+          <select
+            className="select select-primary"
+            name="rounds"
+            onChange={handleCreateFormChange}
+            form="createForm"
+          >
+            <option disabled selected>
+              Rounds
+            </option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+          </select>
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+        >
+          {/* TODO: Separate join/create forms */}
+          Create Game
+        </button>
+      </form>
+      <form
+        id="joinForm"
+        noValidate={true}
+        onSubmit={(e: any) => handleSubmit(e, "join")}
+        className="grid place-items-center shadow-md bg-neutral-50 rounded px-8 pt-6 pb-8 mb-4 gap-2"
+      >
       <label className="form-control w-full max-w-xs">
         <div className="label">
           <span className="label-text">Game ID</span>
-          <span className="label-text-alt">(blank for new game)</span>
         </div>
         <input
           type="text"
           className="input input-bordered input-primary w-full max-w-xs"
           name="game_id"
           maxLength={36}
-          onChange={handleChange}
+          onChange={handleJoinFormChange}
           required
         />
       </label>
-      <label className="form-control w-full max-w-xs">
-        <div className="label">
-          <span className="label-text">Player Name</span>
-        </div>
-        <input
-          type="text"
-          className="input input-bordered input-primary w-full max-w-xs"
-          name="player_name"
-          maxLength={16}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <div className="w-full grid grid-cols-2 grid-rows-1 gap-2">
-        <select
-          className="select select-primary"
-          name="max_players"
-          onChange={handleChange}
-          form="gameform"
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Player Name</span>
+          </div>
+          <input
+            type="text"
+            className="input input-bordered input-primary w-full max-w-xs"
+            name="player_name"
+            maxLength={16}
+            onChange={handleJoinFormChange}
+            required
+          />
+        </label>
+        <button
+          type="submit"
+          className="btn btn-primary"
         >
-          <option disabled selected>
-            Max players
-          </option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-          <option>6</option>
-        </select>
-        <select
-          className="select select-primary"
-          name="rounds"
-          onChange={handleChange}
-          form="gameform"
-        >
-          <option disabled selected>
-            Rounds
-          </option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-          <option>6</option>
-          <option>7</option>
-          <option>8</option>
-          <option>9</option>
-          <option>10</option>
-        </select>
-      </div>
-      <button
-        type="submit"
-        className="btn btn-primary"
-      >
-        {/* TODO: Separate join/create forms */}
-        Join Game
-      </button>
-    </form>
+          {/* TODO: Separate join/create forms */}
+          Join Game
+        </button>
+      </form>
+    </div>
   );
 }
